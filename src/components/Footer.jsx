788793aa350b7b0a3bdd9f1510b2fa703df5d21d
njordulv <njordulv@gmail.com>
2023-11-27 21:25:08 +0200

    ADD: correct date

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,13 @@
-import styles from "../App.module.css"
+import styles from '../App.module.css'
 
 const Footer = () => {
+  const date = new Date()
+  const year = date.getFullYear()
+
   return (
     <footer>
       <div className={`${styles.wrapper} ${styles.FooterWrapper}`}>
-        <div>&copy; 2023 React Fasting App</div>
+        <div>&copy; {year} React Fasting App</div>
         <div>
           Stay motivated and achieve your health goals. Remember, the journey to
           a healthier you starts with the first step. Keep pushing forward, and

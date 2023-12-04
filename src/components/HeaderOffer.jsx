71cc7029e5cb1787b3aebff4157858983073f3d5
njordulv <njordulv@gmail.com>
2023-12-04 20:36:52 +0200

    UPD: starting offer page

--- src/components/HeaderOffer.jsx
+++ src/components/HeaderOffer.jsx
@@ -5,7 +5,8 @@ const HeaderOffer = () => {
   return (
     <header className={styles.sticky}>
       <div className={styles.wrapper}>
-        <div className={styles.container}>
+        <div className={`${styles.container} ${styles.spaceBetween}`}>
+          <div className="timer">00:00:00</div>
           <button className="button">Get My Plan</button>
         </div>
       </div>
--- src/components/Offer/Offer.jsx
+++ src/components/Offer/Offer.jsx
@@ -1,7 +1,47 @@
+import { IoIosBody } from 'react-icons/io'
+import { PiPulseThin } from 'react-icons/pi'
+import styles from './Offer.module.css'
+
 const Offer = () => {
   return (
     <>
       <h1>Offer</h1>
+      <div className={styles.offerTarget}>
+        <div className={styles.offerTargetTop}>
+          <div>
+            <IoIosBody />
+          </div>
+          <div>
+            <IoIosBody />
+          </div>
+        </div>
+        <div className={styles.offerTargetMid}>
+          <b>Before</b>
+          <b>After</b>
+        </div>
+        <div className={styles.offerTargetBot}>
+          <div>
+            <span>Metabolic rate</span>
+            <div className={styles.rate}>
+              <PiPulseThin className={styles.rateVivid} />
+              <PiPulseThin className={styles.rateVivid} />
+              <PiPulseThin className={styles.rateVivid} />
+              <PiPulseThin className={styles.ratePale} />
+              <PiPulseThin className={styles.ratePale} />
+            </div>
+          </div>
+          <div>
+            <span>Metabolic rate</span>
+            <div className={`${styles.rate} ${styles.rateEnd}`}>
+              <PiPulseThin className={styles.rateVivid} />
+              <PiPulseThin className={styles.rateVivid} />
+              <PiPulseThin className={styles.rateVivid} />
+              <PiPulseThin className={styles.rateVivid} />
+              <PiPulseThin className={styles.rateVivid} />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }

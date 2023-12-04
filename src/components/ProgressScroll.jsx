e644c685d6e0e9635ee92d42fdc4141648e6e9a9
njordulv <njordulv@gmail.com>
2023-12-04 18:23:34 +0200

    ADD: progress scroll component

---
+++ src/components/ProgressScroll.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react'
+
+const ProgressScroll = () => {
+  const [scrollProgress, setScrollProgress] = useState(0)
+
+  const calculateScrollProgress = () => {
+    const windowHeight = window.innerHeight
+    const documentHeight = document.documentElement.scrollHeight
+    const scrollTop = window.scrollY
+    const scrollHeight = documentHeight - windowHeight
+    const scrollProgress = (scrollTop / scrollHeight) * 100
+
+    setScrollProgress(scrollProgress)
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', calculateScrollProgress)
+
+    return () => {
+      window.removeEventListener('scroll', calculateScrollProgress)
+    }
+  }, [])
+
+  return (
+    <>
+      <div
+        className="scrollProgressBar"
+        style={{
+          width: `${scrollProgress.toFixed(2)}%`,
+          height: '4px',
+          borderRadius: '2px',
+          backgroundColor: '#91b7d9',
+        }}
+      ></div>
+    </>
+  )
+}
+
+export default ProgressScroll

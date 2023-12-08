06362bfbe19bbd2bb79d0e9391c99a27cd51bcd5
njordulv <njordulv@gmail.com>
2023-12-08 21:32:36 +0200

    ADD: payment form validation

--- src/components/Payment/Payment.jsx
+++ src/components/Payment/Payment.jsx
@@ -1,5 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import {
   setPlan1,
   setPlan2,
@@ -17,6 +19,7 @@ const Payment = () => {
   const plan2 = useSelector(selectPlan2)
   const plan3 = useSelector(selectPlan3)
   const checkbox = useSelector(selectCheckbox)
+  const [errorDisplayed, setErrorDisplayed] = useState(false)
 
   useEffect(() => {
     dispatch(setPlan2('3 months'))
@@ -40,84 +43,97 @@ const Payment = () => {
 
   const submitPaymentHandler = (e) => {
     e.preventDefault()
+    setErrorDisplayed(false)
+
+    if (!(checkbox.conditions && checkbox.terms)) {
+      if (!errorDisplayed) {
+        toast.error('Please check all the checkboxes')
+        setErrorDisplayed(true)
+      }
+    } else {
+      toast.success('Good choice')
+    }
   }
 
   return (
-    <form className={styles.payment} onSubmit={submitPaymentHandler}>
-      <div className={styles.paymentPlans}>
-        <div className={styles.paymentPlan}>
-          <input
-            type="radio"
-            id="plan1"
-            name="planOptions"
-            value={plan1}
-            className={styles.paymentInput}
-            onChange={(e) => updatePlan1(e.target.value)}
-          />
-          <label htmlFor="plan1">1-month plan</label>
+    <>
+      <ToastContainer position="bottom-right" autoClose={2000} />
+      <form className={styles.payment} onSubmit={submitPaymentHandler}>
+        <div className={styles.paymentPlans}>
+          <div className={styles.paymentPlan}>
+            <input
+              type="radio"
+              id="plan1"
+              name="planOptions"
+              value={plan1}
+              className={styles.paymentInput}
+              onChange={(e) => updatePlan1(e.target.value)}
+            />
+            <label htmlFor="plan1">1-month plan</label>
+          </div>
+          <div className={styles.paymentPlan}>
+            <input
+              type="radio"
+              id="plan2"
+              name="planOptions"
+              value={plan2}
+              className={styles.paymentInput}
+              onChange={(e) => updatePlan2(e.target.value)}
+              defaultChecked
+            />
+            <label htmlFor="plan2">3-month plan</label>
+          </div>
+          <div className={styles.paymentPlan}>
+            <input
+              type="radio"
+              id="plan3"
+              name="planOptions"
+              value={plan3}
+              className={styles.paymentInput}
+              onChange={(e) => updatePlan3(e.target.value)}
+            />
+            <label htmlFor="plan3">6-month plan</label>
+          </div>
         </div>
-        <div className={styles.paymentPlan}>
-          <input
-            type="radio"
-            id="plan2"
-            name="planOptions"
-            value={plan2}
-            className={styles.paymentInput}
-            onChange={(e) => updatePlan2(e.target.value)}
-            defaultChecked
-          />
-          <label htmlFor="plan2">3-month plan</label>
+        <div className={styles.paymentCheckboxes}>
+          <label htmlFor="conditions" className={styles.paymentCheckboxLabel}>
+            <input
+              type="checkbox"
+              name="conditions"
+              checked={checkbox.conditions}
+              onChange={(e) => handleCheckboxChange(e, 'conditions')}
+            />
+            <small>
+              By ticking this box, I acknowledge and accept the Terms and
+              Conditions as well as the Refund Policy
+            </small>
+          </label>
+          <label htmlFor="terms" className={styles.paymentCheckboxLabel}>
+            <input
+              type="checkbox"
+              name="terms"
+              checked={checkbox.terms}
+              onChange={(e) => handleCheckboxChange(e, 'terms')}
+            />
+            <small>
+              By selecting this option, I provide consent for the automatic
+              renewal of my subscription using the specified card. I am aware
+              that today I will be charged 36.73 USD and 78.15 USD for each
+              subsequent quarterly renewal until I opt to cancel. To avoid any
+              charges, it's necessary to cancel your subscription at least one
+              day before its expiration. This can be done by contacting
+              support@fasting.app or calling our US number: 555-01-39. The
+              transaction details might appear on your bank statement
+            </small>
+          </label>
         </div>
-        <div className={styles.paymentPlan}>
-          <input
-            type="radio"
-            id="plan3"
-            name="planOptions"
-            value={plan3}
-            className={styles.paymentInput}
-            onChange={(e) => updatePlan3(e.target.value)}
-          />
-          <label htmlFor="plan3">6-month plan</label>
+        <div className="text-center">
+          <button type="submit" className="button">
+            Get My Plan
+          </button>
         </div>
-      </div>
-      <div className={styles.paymentCheckboxes}>
-        <label htmlFor="conditions" className={styles.paymentCheckboxLabel}>
-          <input
-            type="checkbox"
-            name="conditions"
-            checked={checkbox.conditions}
-            onChange={(e) => handleCheckboxChange(e, 'conditions')}
-          />
-          <small>
-            By ticking this box, I acknowledge and accept the Terms and
-            Conditions as well as the Refund Policy
-          </small>
-        </label>
-        <label htmlFor="terms" className={styles.paymentCheckboxLabel}>
-          <input
-            type="checkbox"
-            name="terms"
-            checked={checkbox.terms}
-            onChange={(e) => handleCheckboxChange(e, 'terms')}
-          />
-          <small>
-            By selecting this option, I provide consent for the automatic
-            renewal of my subscription using the specified card. I am aware that
-            today I will be charged 36.73 USD and 78.15 USD for each subsequent
-            quarterly renewal until I opt to cancel. To avoid any charges, it's
-            necessary to cancel your subscription at least one day before its
-            expiration. This can be done by contacting support@fasting.app or
-            calling our US number: 555-01-39. The transaction details might
-            appear on your bank statement
-          </small>
-        </label>
-      </div>
-      <div className="text-center">
-        <button type="submit" className="button">
-          Get My Plan
-        </button>
-      </div>
-    </form>
+      </form>
+    </>
   )
 }
 

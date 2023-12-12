dad67507572f87248dceb867db5118eea793d13d
njordulv <njordulv@gmail.com>
2023-12-12 21:17:45 +0200

    UPD: checkout form validation

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,62 +1,155 @@
 import { useForm } from 'react-hook-form'
-import { yupResolver } from '@hookform/resolvers/yup'
-import * as yup from 'yup'
 import { useState } from 'react'
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import Plans from './Plans'
 import styles from './Form.module.css'
 
 const Checkout = () => {
+  const currentYear = new Date().getFullYear()
+  const [cardValue, setCardValue] = useState('')
+  const [expDateValue, setExpDateValue] = useState('')
+  const [cvvValue, setCvvValue] = useState('')
+  const [firstNameValue, setFirstNameValue] = useState('')
+  const [lastNameValue, setLastNameValue] = useState('')
   const [emailValue, setEmailValue] = useState('')
-  const [isError, setIsError] = useState('')
-
-  const formValidation = yup.object().shape({
-    cardNumber: yup
-      .string()
-      .label('Card number')
-      .required('Card Number is required'),
-    cardDate: yup.string().label('Card date').required('Card date is required'),
-    cardCode: yup
-      .string()
-      .label('Card code')
-      .required('Security code is required'),
-    firstName: yup
-      .string()
-      .label('First Name')
-      .required('First Name is required'),
-    lastName: yup.string().label('Last Name').required('Last Name is required'),
-    email: yup
-      .string()
-      .email('Invalid email address')
-      .required('Please enter a valid email'),
-  })
 
   const {
     register,
     handleSubmit,
     clearErrors,
     formState: { errors },
-  } = useForm({
-    resolver: yupResolver(formValidation),
-  })
+  } = useForm()
+
+  const cardValidation = {
+    required: 'Credit card number is required',
+    pattern: {
+      value: /^[0-9]{16}$/,
+      message: 'Invalid credit card number',
+    },
+  }
+
+  const expirationDateValidation = {
+    required: 'Expiration date is required',
+    pattern: {
+      value: /^(0[1-9]|1[0-2])\/\d{2}$/,
+      message: 'Invalid expiration date',
+    },
+    validate: (value) => {
+      const enteredYear = Number(value.split('/')[1])
+      const currentYearLastTwoDigits = Number(String(currentYear).slice(-2))
+      const maxAllowedYear = currentYearLastTwoDigits + 10
+
+      if (
+        enteredYear < currentYearLastTwoDigits ||
+        enteredYear > maxAllowedYear
+      ) {
+        return 'Invalid expiration date'
+      }
+
+      return true
+    },
+  }
+
+  const cvvValidation = {
+    required: 'CVV/CVC is required',
+    pattern: {
+      value: /^[0-9]{3,4}$/,
+      message: 'Invalid CVV/CVC',
+    },
+  }
+
+  const firstNameValidation = {
+    required: 'First Name is required',
+    pattern: {
+      value: /^[A-Za-zÀ-ÖØ-öø-ÿ'\- ]+$/,
+      message: 'Invalid name format',
+    },
+  }
+
+  const lastNameValidation = {
+    required: 'Last Name is required',
+    pattern: {
+      value: /^[A-Za-zÀ-ÖØ-öø-ÿ'\- ]+$/,
+      message: 'Invalid name format',
+    },
+  }
+
+  const emailValidation = {
+    required: 'Email is required',
+    pattern: {
+      value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+      message: 'Wrong email address',
+    },
+  }
+
+  const cardHandler = (e) => {
+    const value = e.target.value
+    setCardValue(value)
+
+    if (!value) {
+      clearErrors('cardNumber')
+    }
+  }
+
+  const expDateHandler = (e) => {
+    let value = e.target.value
+    const lastChar = value.charAt(value.length - 1)
+
+    if (value.length === 3 && lastChar === '/') {
+      value = value.substring(0, 2)
+    }
+
+    if (value.length === 2 && lastChar !== '/') {
+      value += '/'
+    }
+
+    if (!value) {
+      clearErrors('expDate')
+    }
+
+    setExpDateValue(value)
+  }
+
+  const cvvHandler = (e) => {
+    const value = e.target.value
+    setCvvValue(value)
+
+    if (!value) {
+      clearErrors('cvv')
+    }
+  }
+
+  const firstNameHandler = (e) => {
+    const value = e.target.value
+    setFirstNameValue(value)
+
+    if (!value) {
+      clearErrors('firstName')
+    }
+  }
+
+  const lastNameHandler = (e) => {
+    const value = e.target.value
+    setLastNameValue(value)
+
+    if (!value) {
+      clearErrors('lastName')
+    }
+  }
 
   const emailHandler = (e) => {
     const value = e.target.value
     setEmailValue(value)
 
-    if (value.length > 1) {
-      clearErrors()
+    if (!value) {
+      clearErrors('email')
     }
   }
 
-  const checkoutFormSubmit = (e) => {
-    e.preventDefault()
-    const value = e.target.value
-
-    if (!value) {
-      setIsError('is-error')
-    }
-
-    return console.log(e)
+  const checkoutFormSubmit = (data) => {
+    console.log(data)
+    toast.success('Form submitted successfully!')
   }
 
   return (
@@ -73,11 +166,13 @@ const Checkout = () => {
                   }`}
                 >
                   <input
-                    {...register('cardNumber')}
+                    {...register('cardNumber', cardValidation)}
                     className={`${styles.input}`}
                     type="text"
                     maxLength="16"
                     placeholder="Card Number"
+                    value={cardValue}
+                    onChange={cardHandler}
                   />
                 </div>
                 {errors.cardNumber && (
@@ -89,19 +184,21 @@ const Checkout = () => {
               <label className={`${styles.inputField}`}>
                 <div
                   className={`${styles.inputHolder} ${
-                    errors.cardDate && `${styles.isError}`
+                    errors.expDate && `${styles.isError}`
                   }`}
                 >
                   <input
-                    {...register('cardDate')}
+                    {...register('expDate', expirationDateValidation)}
                     className={`${styles.input}`}
                     type="text"
-                    maxLength="4"
+                    maxLength="5"
                     placeholder="MM/YY"
+                    value={expDateValue}
+                    onChange={expDateHandler}
                   />
-                  {errors.cardDate && (
+                  {errors.expDate && (
                     <div className={styles.inputError}>
-                      {errors.cardDate.message}
+                      {errors.expDate.message}
                     </div>
                   )}
                 </div>
@@ -109,21 +206,21 @@ const Checkout = () => {
               <label className={`${styles.inputField}`}>
                 <div
                   className={`${styles.inputHolder} ${
-                    errors.cardCode && `${styles.isError}`
+                    errors.cvv && `${styles.isError}`
                   }`}
                 >
                   <input
-                    {...register('cardCode')}
+                    {...register('cvv', cvvValidation)}
                     className={`${styles.input}`}
                     type="password"
                     maxLength="4"
                     placeholder="CVV/CVC"
+                    value={cvvValue}
+                    onChange={cvvHandler}
                   />
                 </div>
-                {errors.cardCode && (
-                  <div className={styles.inputError}>
-                    {errors.cardCode.message}
-                  </div>
+                {errors.cvv && (
+                  <div className={styles.inputError}>{errors.cvv.message}</div>
                 )}
               </label>
             </div>
@@ -135,10 +232,12 @@ const Checkout = () => {
                   }`}
                 >
                   <input
-                    {...register('firstName')}
+                    {...register('firstName', firstNameValidation)}
                     type="text"
                     className={`${styles.input}`}
                     placeholder="First name"
+                    value={firstNameValue}
+                    onChange={firstNameHandler}
                   />
                 </div>
                 {errors.firstName && (
@@ -154,10 +253,12 @@ const Checkout = () => {
                   }`}
                 >
                   <input
-                    {...register('lastName')}
+                    {...register('lastName', lastNameValidation)}
                     type="text"
                     className={`${styles.input}`}
                     placeholder="Last name"
+                    value={lastNameValue}
+                    onChange={lastNameHandler}
                   />
                 </div>
                 {errors.lastName && (
@@ -173,7 +274,7 @@ const Checkout = () => {
                   }`}
                 >
                   <input
-                    {...register('email')}
+                    {...register('email', emailValidation)}
                     type="text"
                     className={`${styles.input}`}
                     placeholder="Email"
@@ -190,6 +291,11 @@ const Checkout = () => {
             </div>
           </div>
         </form>
+        <ToastContainer
+          position="bottom-right"
+          autoClose={3000}
+          theme="colored"
+        />
       </section>
       <Plans />
       <br />
